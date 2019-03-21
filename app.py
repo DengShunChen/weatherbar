9e368eede83f89bf345d05958156338c8571eec0
DengShunChen <dschen@cwb.gov.tw>
2019-03-21 12:11:13 +0800

    add figure message

--- app.py
+++ app.py
@@ -33,7 +33,11 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    #message = TextSendMessage(text=event.message.text)
+    message = ImageSendMessage(
+    original_content_url='https://www.cwb.gov.tw/Data/radar/CV1_TW_1000.png',
+    preview_image_url='https://www.cwb.gov.tw/Data/temperature/temp.jpg'
+    )
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
