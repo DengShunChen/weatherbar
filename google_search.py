418f67ddb0e487831a6d1477707baa005bd2f2f3
DengShunChen <dschen@cwb.gov.tw>
2019-05-06 15:37:39 +0800

    make it better

--- google_search.py
+++ google_search.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python 
 from googlesearch import search as gs 
+import requests
+import urllib.request as ur
+from bs4 import BeautifulSoup
 
 def search(query):
 
+
+  def google_scrape(url):
+    thepage = ur.urlopen(url)
+    soup = BeautifulSoup(thepage, "html.parser")
+    return soup.title.text
+
+  i = 1
+  strings = ''
+  for url in gs(query, stop=5):
+    a = google_scrape(url)
+    strings = strings + str(i) + ". " + a + '\n'
+    strings = strings + url + '\n'
+    strings = strings + '\n' 
+    i += 1
+
+  return strings
+
+if __name__ == '__main__':
   # to search 
-  #query = "郭文貴"
+  query = "郭文貴"
 
-  string=''
-  for j in gs(query, stop=5 ): 
-    print(j) 
-    string = string + j + '\n'
-
-  return string
+  content = search(query) 
+  print(content)
